5cd7d47161bcd34a7e0351a7b2a823d1a53a4775
ShinGwangCheon <58453569+Shin-GC@users.noreply.github.com>
2021-11-21 21:21:10 +0900

    함수를 사용하여 반복적인 행동 취하기

--- Tutorial/index.js
+++ Tutorial/index.js
@@ -1,16 +1,10 @@
-// 숫자형과 문자열 재료
-let material1 = 3;
-let material2 = '3';
-let material3 = 10;
-let material4 = '4';
-let material5 = 4;
+function logParticipant(name){
+  console.log(`${name}(이)가 대화에 참여했습니다.`)
+}
 
-// 연산 결과
-let result1 = material2 + material4;
-let result2 = material3 * material1 + material5;
-
-// 테스트 코드
-console.log(result1);
-console.log(typeof result1);
-console.log(result2);
-console.log(typeof result2);
+logParticipant('동수');
+logParticipant('윤하');
+logParticipant('재준');
+logParticipant('동훈');
+logParticipant('영희');
+logParticipant('신욱');
