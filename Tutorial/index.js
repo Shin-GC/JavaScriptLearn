15d6e1f2f8fa05ac6f98b569c1d8466722045be2
ShinGwangCheon <58453569+Shin-GC@users.noreply.github.com>
2021-11-21 23:55:25 +0900

    while문 사용법

--- Tutorial/index.js
+++ Tutorial/index.js
@@ -174,20 +174,29 @@
 //     }
 // }
 
-function printTriangle(height) {
-    let message = ''
-    for (let i = 0; i < height; i++){
-        message += '*';
-        console.log(`${message}`);
+// function printTriangle(height) {
+//     let message = ''
+//     for (let i = 0; i < height; i++){
+//         message += '*';
+//         console.log(`${message}`);
+//     }
+// }
+//
+// // 테스트 코드
+// console.log('높이: 1');
+// printTriangle(1);
+//
+// console.log('높이: 3');
+// printTriangle(3);
+//
+// console.log('높이: 5');
+// printTriangle(5);
+
+let i = 0;
+while (i <= 100){
+    if (i % 2 !== 0){
+        console.log(i);
+        i++;
     }
+    i++;
 }
-
-// 테스트 코드
-console.log('높이: 1');
-printTriangle(1);
-
-console.log('높이: 3');
-printTriangle(3);
-
-console.log('높이: 5');
-printTriangle(5);
