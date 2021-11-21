77ce3f82f5fe975f022051a455636f7207823167
ShinGwangCheon <58453569+Shin-GC@users.noreply.github.com>
2021-11-21 22:47:59 +0900

    switch문

--- Tutorial/index.js
+++ Tutorial/index.js
@@ -89,49 +89,81 @@
 // printGrade(29, 24);
 // printGrade(37, 42);
 
-// 나의 나이와, 나의 성별을 저장하는 변수
-let myAge = 26;
-let myGender = 'male';
+// // 나의 나이와, 나의 성별을 저장하는 변수
+// let myAge = 26;
+// let myGender = 'male';
+//
+// // 호칭을 담은 변수
+// let callOlderBrother = '형';
+// let callOlderSister = '누나';
+// let callFriend = '친구';
+// let callYoungerSister = '여동생';
+// let callYoungerBrother = '남동생';
+//
+// // 상대방의 나이와 성별에 따른 호칭을 리턴하는 함수 whatShouldICall를 완성하세요.
+// function whatShouldICallYou(yourAge, yourGender) {
+//     if (myAge === yourAge){
+//         return "친구"
+//     }
+//     else if (myAge >= yourAge){
+//         if (yourGender === 'female'){
+//             return '여동생'
+//         }
+//         else{
+//             return '남동생'
+//         }
+//     }
+//     else{
+//         if (yourGender === 'female'){
+//             return '누나'
+//         }
+//         else{
+//             return '형'
+//         }
+//     }
+// }
+//
+// // 테스트 코드
+// let result1 = whatShouldICallYou(25, 'female');
+// let result2 = whatShouldICallYou(20, 'male');
+// let result3 = whatShouldICallYou(26, 'female');
+// let result4 = whatShouldICallYou(30, 'male');
+// let result5 = whatShouldICallYou(31, 'female');
+//
+// console.log(result1);
+// console.log(result2);
+// console.log(result3);
+// console.log(result4);
+// console.log(result5);
 
-// 호칭을 담은 변수
-let callOlderBrother = '형';
-let callOlderSister = '누나';
-let callFriend = '친구';
-let callYoungerSister = '여동생';
-let callYoungerBrother = '남동생';
+// 각 등급별 가격
+let VIPPrice = 15;
+let RPrice = 13;
+let SPrice = 10;
+let APrice = 8;
 
-// 상대방의 나이와 성별에 따른 호칭을 리턴하는 함수 whatShouldICall를 완성하세요.
-function whatShouldICallYou(yourAge, yourGender) {
-    if (myAge === yourAge){
-        return "친구"
-    }
-    else if (myAge >= yourAge){
-        if (yourGender === 'female'){
-            return '여동생'
-        }
-        else{
-            return '남동생'
-        }
-    }
-    else{
-        if (yourGender === 'female'){
-            return '누나'
-        }
-        else{
-            return '형'
-        }
+function checkPrice(grade) {
+    switch(grade){
+        case 'R':
+            console.log(`R석은 ${RPrice}만원 입니다.`)
+            break;
+        case 'VIP':
+        console.log(`VIP석은 ${VIPPrice}만원 입니다.`)
+        break;
+        case 'S':
+        console.log(`S석은 ${SPrice}만원 입니다.`)
+        break;
+        case 'A':
+        console.log(`A석은 ${APrice}만원 입니다.`)
+        break;
+        default:
+            console.log('VIP, R, S, A 중에서 하나를 선택해 주세요.')
     }
 }
 
 // 테스트 코드
-let result1 = whatShouldICallYou(25, 'female');
-let result2 = whatShouldICallYou(20, 'male');
-let result3 = whatShouldICallYou(26, 'female');
-let result4 = whatShouldICallYou(30, 'male');
-let result5 = whatShouldICallYou(31, 'female');
-
-console.log(result1);
-console.log(result2);
-console.log(result3);
-console.log(result4);
-console.log(result5);
+checkPrice('R');
+checkPrice('VIP');
+checkPrice('S');
+checkPrice('A');
+checkPrice('B');
