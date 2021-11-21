f3510a9fecd9b84cc79e3621ab9560b953d53c04
ShinGwangCheon <58453569+Shin-GC@users.noreply.github.com>
2021-11-21 21:20:06 +0900

    연산 부호 이해 확인하기

--- Tutorial/index.js
+++ Tutorial/index.js
@@ -1,10 +1,16 @@
-function calcWage(name, time, wage) {
-  let total = time * wage;
+// 숫자형과 문자열 재료
+let material1 = 3;
+let material2 = '3';
+let material3 = 10;
+let material4 = '4';
+let material5 = 4;
 
-  console.log(`${name}님의 근무 시간은 총 ${time}시간이며, 최종 급여는 ${wage * time}원 입니다.`);
-}
+// 연산 결과
+let result1 = material2 + material4;
+let result2 = material3 * material1 + material5;
 
-calcWage('김윤식', 208, 11340);
-calcWage('성규재', 175, 12160);
-calcWage('손태웅', 161, 13070);
-calcWage('허우선', 222, 10980);
+// 테스트 코드
+console.log(result1);
+console.log(typeof result1);
+console.log(result2);
+console.log(typeof result2);
