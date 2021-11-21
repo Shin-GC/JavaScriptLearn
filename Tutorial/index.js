180a1b63d469dcb71142e194c002b31aabf9b636
ShinGwangCheon <58453569+Shin-GC@users.noreply.github.com>
2021-11-21 22:39:51 +0900

    조건문 if, else if

--- Tutorial/index.js
+++ Tutorial/index.js
@@ -63,28 +63,75 @@
 // checkHeight(170);
 
 
-function printGrade(midtermScore, finalScore){
-	let totalScore = midtermScore + finalScore;
+// function printGrade(midtermScore, finalScore){
+// 	let totalScore = midtermScore + finalScore;
+//
+//     if (totalScore >= 90){
+//         console.log('A')
+//     }
+//     else if(totalScore >= 80){
+//         console.log('B')
+//     }
+//     else if(totalScore >= 70){
+//         console.log('C')
+//     }
+//     else if(totalScore >= 60){
+//         console.log('D')
+//     }
+//     else if(totalScore < 60){
+//         console.log('F')
+//     }
+// }
+//
+// // 테스트 코드
+// printGrade(25, 35);
+// printGrade(50, 45);
+// printGrade(29, 24);
+// printGrade(37, 42);
 
-    if (totalScore >= 90){
-        console.log('A')
+// 나의 나이와, 나의 성별을 저장하는 변수
+let myAge = 26;
+let myGender = 'male';
+
+// 호칭을 담은 변수
+let callOlderBrother = '형';
+let callOlderSister = '누나';
+let callFriend = '친구';
+let callYoungerSister = '여동생';
+let callYoungerBrother = '남동생';
+
+// 상대방의 나이와 성별에 따른 호칭을 리턴하는 함수 whatShouldICall를 완성하세요.
+function whatShouldICallYou(yourAge, yourGender) {
+    if (myAge === yourAge){
+        return "친구"
     }
-    else if(totalScore >= 80){
-        console.log('B')
+    else if (myAge >= yourAge){
+        if (yourGender === 'female'){
+            return '여동생'
+        }
+        else{
+            return '남동생'
+        }
     }
-    else if(totalScore >= 70){
-        console.log('C')
-    }
-    else if(totalScore >= 60){
-        console.log('D')
-    }
-    else if(totalScore < 60){
-        console.log('F')
+    else{
+        if (yourGender === 'female'){
+            return '누나'
+        }
+        else{
+            return '형'
+        }
     }
 }
 
 // 테스트 코드
-printGrade(25, 35);
-printGrade(50, 45);
-printGrade(29, 24);
-printGrade(37, 42);
+let result1 = whatShouldICallYou(25, 'female');
+let result2 = whatShouldICallYou(20, 'male');
+let result3 = whatShouldICallYou(26, 'female');
+let result4 = whatShouldICallYou(30, 'male');
+let result5 = whatShouldICallYou(31, 'female');
+
+console.log(result1);
+console.log(result2);
+console.log(result3);
+console.log(result4);
+console.log(result5);
