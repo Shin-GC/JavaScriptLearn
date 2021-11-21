186281e7c3c90354eecabda802b18f134fb4e3ed
ShinGwangCheon <58453569+Shin-GC@users.noreply.github.com>
2021-11-21 22:52:53 +0900

    for 문 사용법

--- Tutorial/index.js
+++ Tutorial/index.js
@@ -137,33 +137,39 @@
 // console.log(result5);
 
 // 각 등급별 가격
-let VIPPrice = 15;
-let RPrice = 13;
-let SPrice = 10;
-let APrice = 8;
+// let VIPPrice = 15;
+// let RPrice = 13;
+// let SPrice = 10;
+// let APrice = 8;
+//
+// function checkPrice(grade) {
+//     switch(grade){
+//         case 'R':
+//             console.log(`R석은 ${RPrice}만원 입니다.`)
+//             break;
+//         case 'VIP':
+//         console.log(`VIP석은 ${VIPPrice}만원 입니다.`)
+//         break;
+//         case 'S':
+//         console.log(`S석은 ${SPrice}만원 입니다.`)
+//         break;
+//         case 'A':
+//         console.log(`A석은 ${APrice}만원 입니다.`)
+//         break;
+//         default:
+//             console.log('VIP, R, S, A 중에서 하나를 선택해 주세요.')
+//     }
+// }
+//
+// // 테스트 코드
+// checkPrice('R');
+// checkPrice('VIP');
+// checkPrice('S');
+// checkPrice('A');
+// checkPrice('B');
 
-function checkPrice(grade) {
-    switch(grade){
-        case 'R':
-            console.log(`R석은 ${RPrice}만원 입니다.`)
-            break;
-        case 'VIP':
-        console.log(`VIP석은 ${VIPPrice}만원 입니다.`)
-        break;
-        case 'S':
-        console.log(`S석은 ${SPrice}만원 입니다.`)
-        break;
-        case 'A':
-        console.log(`A석은 ${APrice}만원 입니다.`)
-        break;
-        default:
-            console.log('VIP, R, S, A 중에서 하나를 선택해 주세요.')
+for (let i = 1; i <= 100; i++){
+    if (i % 2 === 0){
+        console.log(`${i}`)
     }
 }
-
-// 테스트 코드
-checkPrice('R');
-checkPrice('VIP');
-checkPrice('S');
-checkPrice('A');
-checkPrice('B');
