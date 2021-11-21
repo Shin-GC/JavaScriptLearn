360bfcc525d1df338e40f4a6ca7a036dce0a0a3b
ShinGwangCheon <58453569+Shin-GC@users.noreply.github.com>
2021-11-21 15:43:54 +0900

    JS 학습 1일차 함수 return 사용하기

--- Tutorial/index.js
+++ Tutorial/index.js
@@ -1,7 +1,17 @@
-function bmiCalculator(name, weight, tall){
-    console.log(name + "님의 체질량 지수는 " + weight / (tall * tall / 10000) + "입니다.")
+function interestCalculator(money, term, rate){
+    return money * term * (rate / 100)
 }
 
-bmiCalculator('홀쭉이', 43.52, 160);
-bmiCalculator('코린이', 61.25, 175);
-bmiCalculator('통통이', 77.76, 180);
+// 조건 입력 테스트
+let myMoney = 3650000; // 맡긴 금액 (원)
+let saveTerm = 1; // 맡기는 기간 (년)
+let interestRate = 4; // 이자율 (%)
+
+// 수령액 계산 테스트
+let myInterest = interestCalculator(myMoney, saveTerm, interestRate);
+let totalMoney = myMoney + myInterest;
+
+// 출력 테스트
+console.log('맡긴 금액은 ' + myMoney + '원 입니다.');
+console.log('이자는 ' + myInterest + '원 입니다.');
+console.log('최종 받을 금액은 ' + totalMoney + '원 입니다.');
