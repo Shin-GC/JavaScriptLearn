bbac580cafaaa6a6423bb49a5a88637a26f38ace
ShinGwangCheon <58453569+Shin-GC@users.noreply.github.com>
2021-11-21 21:08:22 +0900

    템플릿 문자열 이용하기

--- Tutorial/index.js
+++ Tutorial/index.js
@@ -1,7 +1,10 @@
-let myNumber = 5
+function calcWage(name, time, wage) {
+  let total = time * wage;
 
-function getTwice(x){
-    return x * 2;
+  console.log(`${name}님의 근무 시간은 총 ${time}시간이며, 최종 급여는 ${wage * time}원 입니다.`);
 }
 
-console.log(`${myNumber}의 두배는 ${getTwice(myNumber)} 입니다.`)
+calcWage('김윤식', 208, 11340);
+calcWage('성규재', 175, 12160);
+calcWage('손태웅', 161, 13070);
+calcWage('허우선', 222, 10980);
