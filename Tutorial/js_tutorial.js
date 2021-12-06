17afd281511a78aa426618e249133a7149cacd30
ShinGwangCheon <58453569+Shin-GC@users.noreply.github.com>
2021-12-07 01:38:13 +0900

    [객체 사용] 12-07

--- Tutorial/js_tutorial.js
+++ Tutorial/js_tutorial.js
@@ -219,35 +219,170 @@
 //         console.log(`${i} * ${j} = ${i*j}`)
 //     }
 // }
+//
+// let previous = 1;  // 1
+// let current = 0;  // 2
+// let i = 1;
+// let temp = 0;
+// while(i <= 50){
+//     temp = current;
+//     current += previous;
+//     previous = temp;
+//     console.log(current)
+//     i += 1;
+// }
+//
 
-let previous = 1;  // 1
-let current = 0;  // 2
-let i = 1;
-let temp = 0;
-while(i <= 50){
-    temp = current;
-    current += previous;
-    previous = temp;
-    console.log(current)
-    i += 1;
+// let myVoca = {
+//     'function': '함수',
+//     'variable': '변수',
+//     'constant': '상수',
+//     'local': '지역의',
+//     'global': '전반적인'
+// }
+//
+// delete myVoca.function;
+// delete myVoca.constant;
+// delete myVoca.local;
+//
+// console.log(myVoca);
+// console.log(myVoca.constant);
+//
+// myVoca['extend'] = '확장하다';
+// myVoca['export'] = '내보내다';
+// myVoca['import'] = '불러오다';
+// myVoca['default value'] = '기본값';
+//
+//
+// console.log(myVoca);
+// console.log(myVoca.export);
+// console.log(myVoca['default value'])
+
+// let myVoca = {
+// //     addVoca: function (key, value) {
+// //         myVoca[key] = value
+// //     },
+// //     deleteVoca: function (key){
+// //         delete myVoca[key]
+// //     },
+// //     printVoca: function(key){
+// //         console.log(`"${key}"의 뜻은 "${myVoca[key]}"입니다.`)
+// //     }
+// // };
+// //
+// // myVoca.addVoca('parameter', '매개 변수');
+// // myVoca.addVoca('element', '요소');
+// // myVoca.addVoca('property', '속성');
+// // // console.log(myVoca);
+// // //
+// // // // deleteVoca 메소드 테스트 코드
+// // // myVoca.deleteVoca('parameter');
+// // // myVoca.deleteVoca('element');
+// // // console.log(myVoca);
+// // //
+// // // // printVoca 메소드 테스트 코드
+// // // myVoca.printVoca('property');
+// //
+// //
+// // for (let voca in myVoca) {
+// //     console.log(myVoca[voca])
+// // }
+
+// let hyesoonScore = {
+//     '데이터 모델링의 이해': 10,
+//     '데이터 모델과 성능': 8,
+//     'SQL 기본': 22,
+//     'SQL 활용': 18,
+//     'SQL 최적화 기본 원리': 20,
+// };
+//
+// let minsoonScore = {
+//     '데이터 모델링의 이해': 14,
+//     '데이터 모델과 성능': 8,
+//     'SQL 기본': 12,
+//     'SQL 활용': 4,
+//     'SQL 최적화 기본 원리': 16,
+// };
+//
+// function passChecker(scoreObject) {
+//     let totalScore = 0
+//     for (let score in scoreObject){
+//         totalScore += scoreObject[score]
+//     }
+//
+//     if (totalScore >= 60){
+//         console.log('축하합니다! 합격입니다!')
+//     }
+//     else{
+//         console.log('아쉽지만 불합격입니다..')
+//     }
+// }
+//
+// passChecker(hyesoonScore);
+// passChecker(minsoonScore);
+
+// // 현재시간
+// let myDate = new Date();
+// console.log(myDate)
+//
+// //설정 년월일
+// myDate = new Date('1997-03-24')
+// console.log(myDate.toLocaleString())
+//
+// // 설정 년월일 및 시간
+// myDate = new Date('1997-03-24T15:30:55')
+// console.log(myDate.toLocaleString())
+// //하나씩 인자 주기
+// myDate = new Date(1997,2,24,15,30,40)
+// //이때 month 인자는 0이 1월을 의미하기 때문에 3월을 쓰고 싶다면 2를 적어줘야한다.
+// console.log(myDate.toLocaleString())
+//
+// let today = new Date()
+// let timeDiff = today.getTime() - new Date(2021,11,5,19,13)
+//
+// console.log(`초 : ${timeDiff / 1000}`)
+// console.log(`분: ${timeDiff / 1000 / 60}`)
+// console.log(`시: ${timeDiff/ 1000 / 60 / 60}`)
+// console.log(`일: ${timeDiff / 1000 / 60 / 60 / 24}`)
+// console.log(today.getDay())
+//
+// switch (today.getDay()) {
+//     case 0:
+//         console.log('일요일');
+//         break;
+//     case 1:
+//         console.log('월요일');
+//         break;
+//     case 2:
+//         console.log('화요일');
+//         break;
+//     case 3:
+//         console.log('수요일');
+//         break;
+//     case 4:
+//         console.log('목요일');
+//         break;
+//     case 5:
+//         console.log('금요일');
+//         break;
+//     case 6:
+//         console.log('토요일');
+//         break;
+// }
+// // 일요일부터 월요일까지 0 ~ 6 의 숫자로 반환
+// console.log(today.toLocaleTimeString())
+// console.log(today.toLocaleDateString())
+// console.log(today.toLocaleString())
+// console.log(`${today.getHours()}시 ${today.getMinutes()}분`)
+// console.log(today.getDate())
+
+let today = new Date(2112, 8, 24);
+let jaeSangStart = new Date(2109, 7, 1);
+
+function workDayCalc(startDate) {
+    // 여기에 코드를 작성해 주세요.
+    let timeDiff = today.getTime() - startDate.getTime()
+    console.log(`오늘은 입사한 지 ${(timeDiff / 1000 / 60 / 60 / 24) + 1}일째 되는 날 입니다.`)
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+workDayCalc(jaeSangStart);
---
+++ Tutorial/tistory.js
@@ -0,0 +1,15 @@
+let myChoice = 2
+
+switch (myChoice) {
+    case 1:
+        console.log("1을 선택하셨군요!");
+        break;
+    case 2:
+        console.log("2를 선택하셨군요!");
+        break;
+    case 3:
+        console.log("3을 선택하셨군요!");
+        break;
+    default:
+        console.log('1, 2, 3 중에 선택해주세요!')
+}
